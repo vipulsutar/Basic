a82c2f98e12d0bfc9e48e57ed2ea67247054da54
vipulsutar <98753145+vipulsutar@users.noreply.github.com>
2024-06-28 18:08:00 +0530

    Update Basic.java

--- src/ForGit/Basic.java
+++ src/ForGit/Basic.java
@@ -2,7 +2,7 @@ package ForGit;
 
 public class Basic {
 	public static void main(String[] args){
-		System.out.println("hello");
+		System.out.println("hello world ");
 		System.out.println("doin pull request");
 	}
 }
